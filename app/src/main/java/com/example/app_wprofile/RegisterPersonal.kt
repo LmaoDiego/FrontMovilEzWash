1d8007d7b2584962f3f0dd6595bf2d0e33f2a435
Chamejul <Shamefulspace7@hotmail.com>
2021-10-02 05:27:36 -0500

    Partner Activities

---
+++ app/src/main/java/com/example/app_wprofile/RegisterPersonal.kt
@@ -0,0 +1,11 @@
+package com.example.app_wprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterPersonal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_personal)
+    }
+}
